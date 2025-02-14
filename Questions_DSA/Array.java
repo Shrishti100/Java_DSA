cd5833b0e15c638988d20b4785b73bb905f0ab20
Shrishti Gupta <75877305+Shrishti100@users.noreply.github.com>
2025-02-14 09:03:01 +0000

    FirstCommit

---
+++ Questions_DSA/Array.java
@@ -0,0 +1,14 @@
+public class Array
+{
+    public static void main(String args[])
+    {
+        int arr[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i]=i;
+            System.out.println(arr[i]);
+        }
+        
+
+    }
+}
